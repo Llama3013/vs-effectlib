fc08c31cdf7c862076dfdd40a74f1618ea81f99e
Llama3013 <jamesrdcameron@outlook.com.au>
2026-09-13 16:19:58 +1000

    Effectlib mod

---
+++ CakeBuild/Program.cs
@@ -0,0 +1,114 @@
+using Cake.Common;
+using Cake.Common.IO;
+using Cake.Common.Tools.DotNet;
+using Cake.Common.Tools.DotNet.Clean;
+using Cake.Common.Tools.DotNet.Publish;
+using Cake.Core;
+using Cake.Frosting;
+using Cake.Json;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using Vintagestory.API.Common;
+
+public static class Program
+{
+    public static int Main(string[] args)
+    {
+        return new CakeHost()
+            .UseContext<BuildContext>()
+            .Run(args);
+    }
+}
+
+public class BuildContext : FrostingContext
+{
+    public string BuildConfiguration { get; set; }
+    public bool SkipJsonValidation { get; set; }
+    public string ProjectName { get; } = "EffectLib";
+    public string ResourcesDir { get; } = "resources";
+    public string Version { get; }
+    public string Name { get; }
+
+    public string ProjectPath => $"../{ProjectName}/{ProjectName}.csproj";
+
+    public BuildContext(ICakeContext context)
+        : base(context)
+    {
+        BuildConfiguration = context.Argument("configuration", "Release");
+        SkipJsonValidation = context.Argument("skipJsonValidation", false);
+
+        var modInfo = context.DeserializeJsonFromFile<ModInfo>($"../{ResourcesDir}/modinfo.json");
+        Version = modInfo.Version;
+        Name = modInfo.ModID;
+    }
+}
+
+[TaskName("ValidateJson")]
+public sealed class ValidateJsonTask : FrostingTask<BuildContext>
+{
+    public override void Run(BuildContext context)
+    {
+        if (context.SkipJsonValidation)
+        {
+            return;
+        }
+
+        var jsonFiles = context.GetFiles($"../{context.ResourcesDir}/**/*.json");
+        foreach (var file in jsonFiles)
+        {
+            try
+            {
+                var json = File.ReadAllText(file.FullPath);
+                JToken.Parse(json);
+            }
+            catch (JsonException ex)
+            {
+                throw new Exception($"Validation failed for JSON file: {file.FullPath}{Environment.NewLine}{ex.Message}", ex);
+            }
+        }
+    }
+}
+
+[TaskName("Build")]
+[IsDependentOn(typeof(ValidateJsonTask))]
+public sealed class BuildTask : FrostingTask<BuildContext>
+{
+    public override void Run(BuildContext context)
+    {
+        context.DotNetClean(context.ProjectPath,
+            new DotNetCleanSettings
+            {
+                Configuration = context.BuildConfiguration
+            });
+
+        context.DotNetPublish(context.ProjectPath,
+            new DotNetPublishSettings
+            {
+                Configuration = context.BuildConfiguration
+            });
+    }
+}
+
+[TaskName("Package")]
+[IsDependentOn(typeof(BuildTask))]
+public sealed class PackageTask : FrostingTask<BuildContext>
+{
+    public override void Run(BuildContext context)
+    {
+        context.EnsureDirectoryExists("../Releases");
+        context.CleanDirectory("../Releases");
+
+        context.EnsureDirectoryExists($"../Releases/{context.Name}");
+        context.CopyFiles($"../{context.ProjectName}/bin/{context.BuildConfiguration}/Mods/mod/publish/*", $"../Releases/{context.Name}");
+        context.CopyDirectory($"../{context.ResourcesDir}", $"../Releases/{context.Name}/");
+        context.Zip($"../Releases/{context.Name}", $"../Releases/{context.Name}_{context.Version}.zip");
+    }
+}
+
+[TaskName("Default")]
+[IsDependentOn(typeof(PackageTask))]
+public class DefaultTask : FrostingTask
+{
+}
